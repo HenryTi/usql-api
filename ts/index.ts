b0941ef98abe12371e08d24968c624e738af4a9c
Henry <liaohengyi@outlook.com>
2018-04-06 21:31:11 +0800

    /usql/:db/hello return db hello

--- ts/index.ts
+++ ts/index.ts
@@ -19,12 +19,12 @@ let authDebug = new Auth(['*']).middlewareDebug();
 let authUnitx = new Auth(['*']).middlewareUnitx();
 
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  //res.send(err)
-  res.render('error', {
-    message: err.message,
-    error: err
-  });
+    res.status(err.status || 500);
+    //res.send(err)
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
 });
 app.use(bodyParser.json());
 app.use(cors());
@@ -34,16 +34,16 @@ app.set('json replacer', (key, value) => {
 });
 
 app.use(async (req:express.Request, res:express.Response, next:express.NextFunction) => {
-  let s= req.socket;
-  let p = '';
-  if (req.method !== 'GET') p = JSON.stringify(req.body);
-  console.log('%s:%s - %s %s %s', s.remoteAddress, s.remotePort, req.method, req.originalUrl, p);
-  try {
-      await next();
-  }
-  catch (e) {
-      console.error(e);
-  }
+    let s= req.socket;
+    let p = '';
+    if (req.method !== 'GET') p = JSON.stringify(req.body);
+    console.log('%s:%s - %s %s %s', s.remoteAddress, s.remotePort, req.method, req.originalUrl, p);
+    try {
+        await next();
+    }
+    catch (e) {
+        console.error(e);
+    }
 });
 
 //app.use('/api', routers);
@@ -55,8 +55,13 @@ app.use('/usql/:db/tv', [authCheck, tv]);
 // debug tonva usql, 默认 unit=-99, user=-99, 以后甚至可以加访问次数，超过1000次，关闭这个接口
 app.use('/usql/:db/debug', [authDebug, tv]);
 
+app.use('/usql/:db/hello', (req:Request, res:Response) => {
+    let db = req.params.db;
+    res.json({"hello": 'usql-api: hello, db is ' + db});
+});
+
 app.use('/usql/hello', (req:Request, res:Response) => {
-  res.json({"hello": 'usql-api - 中文测试'});
+    res.json({"hello": 'usql-api: hello, it\'s good'});
 });
 
 (app as any).ws('/usql', wsOnConnected);
@@ -100,10 +105,10 @@ app.listen(port, async ()=>{
 });
 
 queue.add({job: undefined})
-  .then(job => {
-    console.log('redis server ok!');
-    return job.remove();
-  })
-  .catch(reason => {
-    console.log('redis server error: ', reason);
-  });
+    .then(job => {
+      console.log('redis server ok!');
+      return job.remove();
+    })
+    .catch(reason => {
+      console.log('redis server error: ', reason);
+    });
