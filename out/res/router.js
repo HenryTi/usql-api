5daeba9e23c5f8b2e8356a7e8a185bdf9677ebbb
Henry <liaohengyi@outlook.com>
2020-08-05 08:57:47 -0400

    upgrade node

--- out/res/router.js
+++ out/res/router.js
@@ -17,12 +17,10 @@ const express_1 = require("express");
 const multer = require("multer");
 const resDb_1 = require("./resDb");
 exports.router = express_1.Router({ mergeParams: true });
-const resPath = 'res-path';
-let resFilesPath;
-let upload;
-let uploadPath;
 function initResPath() {
-    //let dir = __dirname;
+    const resPath = 'res-path';
+    let resFilesPath;
+    let uploadPath;
     if (config.has(resPath) === false) {
         resFilesPath = path.resolve('../res-files');
     }
@@ -33,68 +31,69 @@ function initResPath() {
         fs.mkdirSync(resFilesPath);
     }
     uploadPath = resFilesPath + '/upload/';
-    upload = multer({ dest: uploadPath });
+    let upload = multer({ dest: uploadPath });
+    exports.router.get('/hello', (req, res) => {
+        res.end('hello! ' + req.method + '#' + req.originalUrl);
+    });
+    exports.router.get('/path/:resId', (req, res) => {
+        let resId = req.params['resId'];
+        let p = path.resolve(resFilesPath, resId.replace('-', '/'));
+        res.end('res path: ' + p);
+    });
+    exports.router.get('/:resId', (req, res) => {
+        let resId = req.params['resId'];
+        let p = path.resolve(resFilesPath, resId.replace('-', '/'));
+        res.setHeader('Cache-Control', 'max-age=31557600');
+        let d = new Date;
+        res.setHeader('Expires', new Date(d.getFullYear() + 1, d.getMonth(), d.getDate()).toUTCString());
+        res.sendFile(p);
+    });
+    exports.router.post('/upload', (req, res) => {
+        let s = req.body;
+        let reqHandler = upload.any();
+        reqHandler(req, res, function (err) {
+            return __awaiter(this, void 0, void 0, function* () {
+                if (err) {
+                    res.json({ 'error': 'error' });
+                    return;
+                }
+                let file0 = req.files[0];
+                let { filename, originalname, mimetype } = file0;
+                let path = uploadPath + filename;
+                let resDbRunner = yield resDb_1.getResDbRunner();
+                let ret = yield resDbRunner.procCall('createItem', [originalname, mimetype]);
+                let id = ret[0].id;
+                let dir = String(Math.floor(id / 10000));
+                let file = String(10000 + (id % 10000)).substr(1);
+                let dirPath = resFilesPath + '/' + dir;
+                if (fs.existsSync(dirPath) === false) {
+                    fs.mkdirSync(dirPath);
+                }
+                let pos = originalname.lastIndexOf('.');
+                let suffix;
+                if (pos >= 0)
+                    suffix = originalname.substr(pos);
+                let toPath = dirPath + '/' + file + suffix;
+                yield copyFile(path, toPath);
+                res.json({
+                    ok: true,
+                    res: { id: dir + '-' + file + suffix }
+                });
+                return;
+            });
+        });
+    });
+    function copyFile(from, to) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return new Promise((resolve, reject) => {
+                let source = fs.createReadStream(from);
+                let dest = fs.createWriteStream(to);
+                source.on('end', function () { /* copied */ resolve(); });
+                source.on('error', function (err) { /* error */ reject(err); });
+                source.pipe(dest);
+            });
+        });
+    }
 }
 exports.initResPath = initResPath;
-exports.router.get('/hello', (req, res) => {
-    res.end('hello! ' + req.method + '#' + req.originalUrl);
-});
-exports.router.get('/path/:resId', (req, res) => {
-    let resId = req.params['resId'];
-    let p = path.resolve(resFilesPath, resId.replace('-', '/'));
-    res.end('res path: ' + p);
-});
-exports.router.get('/:resId', (req, res) => {
-    let resId = req.params['resId'];
-    let p = path.resolve(resFilesPath, resId.replace('-', '/'));
-    res.setHeader('Cache-Control', 'max-age=31557600');
-    let d = new Date;
-    res.setHeader('Expires', new Date(d.getFullYear() + 1, d.getMonth(), d.getDate()).toUTCString());
-    res.sendFile(p);
-});
-exports.router.post('/upload', (req, res) => {
-    let s = req.body;
-    upload.any()(req, res, function (err) {
-        return __awaiter(this, void 0, void 0, function* () {
-            if (err) {
-                res.json({ 'error': 'error' });
-                return;
-            }
-            let file0 = req.files[0];
-            let { filename, originalname, mimetype } = file0;
-            let path = uploadPath + filename;
-            let resDbRunner = yield resDb_1.getResDbRunner();
-            let ret = yield resDbRunner.procCall('createItem', [originalname, mimetype]);
-            let id = ret[0].id;
-            let dir = String(Math.floor(id / 10000));
-            let file = String(10000 + (id % 10000)).substr(1);
-            let dirPath = resFilesPath + '/' + dir;
-            if (fs.existsSync(dirPath) === false) {
-                fs.mkdirSync(dirPath);
-            }
-            let pos = originalname.lastIndexOf('.');
-            let suffix;
-            if (pos >= 0)
-                suffix = originalname.substr(pos);
-            let toPath = dirPath + '/' + file + suffix;
-            yield copyFile(path, toPath);
-            res.json({
-                ok: true,
-                res: { id: dir + '-' + file + suffix }
-            });
-            return;
-        });
-    });
-});
-function copyFile(from, to) {
-    return __awaiter(this, void 0, void 0, function* () {
-        return new Promise((resolve, reject) => {
-            let source = fs.createReadStream(from);
-            let dest = fs.createWriteStream(to);
-            source.on('end', function () { /* copied */ resolve(); });
-            source.on('error', function (err) { /* error */ reject(err); });
-            source.pipe(dest);
-        });
-    });
-}
 //# sourceMappingURL=router.js.map
--- ts/res/router.ts
+++ ts/res/router.ts
@@ -1,18 +1,17 @@
 import * as config from 'config';
 import * as fs from 'fs';
 import * as path from 'path';
-import { Router, Request, Response } from "express";
+import { Router } from "express";
 import * as multer from 'multer';
 import { getResDbRunner } from './resDb';
 
 export const router: Router = Router({ mergeParams: true });
 
-const resPath = 'res-path';
-let resFilesPath: string;
-let upload: multer.Instance;
-let uploadPath: string;
 export function initResPath() {
-    //let dir = __dirname;
+	const resPath = 'res-path';
+	let resFilesPath: string;
+	let uploadPath: string;
+	
     if (config.has(resPath) === false) {
         resFilesPath = path.resolve('../res-files');
     }
@@ -23,66 +22,69 @@ export function initResPath() {
         fs.mkdirSync(resFilesPath);
     }
     uploadPath = resFilesPath + '/upload/';
-    upload = multer({ dest:  uploadPath});
+
+	let upload = multer({ dest:  uploadPath});
+
+	router.get('/hello', (req, res) => {
+		res.end('hello! ' + req.method + '#' +  req.originalUrl);
+	});
+	
+	router.get('/path/:resId', (req, res) => {
+		let resId:string = req.params['resId'];
+		let p = path.resolve(resFilesPath, resId.replace('-', '/'));
+		res.end('res path: ' + p);
+	});
+	
+	router.get('/:resId', (req, res) => {
+		let resId:string = req.params['resId'];
+		let p = path.resolve(resFilesPath, resId.replace('-', '/'));
+		res.setHeader('Cache-Control', 'max-age=31557600');
+		let d = new Date;
+		res.setHeader('Expires', new Date(d.getFullYear()+1, d.getMonth(), d.getDate()).toUTCString());
+		res.sendFile(p);
+	});
+	
+	router.post('/upload', (req, res) => {
+		let s = req.body;
+		let reqHandler = upload.any();
+		reqHandler(req, res, async function(err?:any) {
+			if (err) {
+				res.json({'error': 'error'});
+				return;  
+			}
+			let file0 = req.files[0];
+			let {filename, originalname, mimetype} = file0;
+			let path = uploadPath + filename;
+			let resDbRunner = await getResDbRunner();
+			let ret = await resDbRunner.procCall('createItem', [originalname, mimetype]);
+			let id = ret[0].id;
+			let dir = String(Math.floor(id /10000));
+			let file = String(10000 + (id % 10000)).substr(1);
+			let dirPath = resFilesPath + '/' + dir;
+			if (fs.existsSync(dirPath) === false) {
+				fs.mkdirSync(dirPath);
+			}
+			let pos = (originalname as string).lastIndexOf('.');
+			let suffix:string;
+			if (pos >= 0) suffix = (originalname as string).substr(pos);
+			let toPath = dirPath + '/' + file + suffix;
+			await copyFile(path, toPath);
+			res.json({
+				ok: true,
+				res: {id: dir + '-' + file + suffix}
+			});
+			return;
+		});
+	});
+	
+	async function copyFile(from: string, to: string):Promise<void> {
+		return new Promise<void>((resolve, reject) => {
+			let source = fs.createReadStream(from);
+			let dest = fs.createWriteStream(to);
+			source.on('end', function() { /* copied */ resolve(); });
+			source.on('error', function(err:any) { /* error */ reject(err); });
+			source.pipe(dest);
+		});
+	}
 }
 
-router.get('/hello', (req, res) => {
-    res.end('hello! ' + req.method + '#' +  req.originalUrl);
-});
-
-router.get('/path/:resId', (req, res) => {
-    let resId:string = req.params['resId'];
-    let p = path.resolve(resFilesPath, resId.replace('-', '/'));
-    res.end('res path: ' + p);
-});
-
-router.get('/:resId', (req, res) => {
-    let resId:string = req.params['resId'];
-    let p = path.resolve(resFilesPath, resId.replace('-', '/'));
-    res.setHeader('Cache-Control', 'max-age=31557600');
-    let d = new Date;
-    res.setHeader('Expires', new Date(d.getFullYear()+1, d.getMonth(), d.getDate()).toUTCString());
-    res.sendFile(p);
-});
-
-router.post('/upload', (req, res) => {
-    let s = req.body;
-    upload.any()(req, res, async function(err) {
-        if (err) {
-            res.json({'error': 'error'});
-            return;  
-        }
-        let file0 = req.files[0];
-        let {filename, originalname, mimetype} = file0;
-        let path = uploadPath + filename;
-        let resDbRunner = await getResDbRunner();
-        let ret = await resDbRunner.procCall('createItem', [originalname, mimetype]);
-        let id = ret[0].id;
-        let dir = String(Math.floor(id /10000));
-        let file = String(10000 + (id % 10000)).substr(1);
-        let dirPath = resFilesPath + '/' + dir;
-        if (fs.existsSync(dirPath) === false) {
-            fs.mkdirSync(dirPath);
-        }
-        let pos = (originalname as string).lastIndexOf('.');
-        let suffix:string;
-        if (pos >= 0) suffix = (originalname as string).substr(pos);
-        let toPath = dirPath + '/' + file + suffix;
-        await copyFile(path, toPath);
-        res.json({
-            ok: true,
-            res: {id: dir + '-' + file + suffix}
-        });
-        return;
-    });
-});
-
-async function copyFile(from: string, to: string):Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-        let source = fs.createReadStream(from);
-        let dest = fs.createWriteStream(to);
-        source.on('end', function() { /* copied */ resolve(); });
-        source.on('error', function(err:any) { /* error */ reject(err); });
-        source.pipe(dest);
-    });
-}
