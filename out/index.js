57dcaf0165d2ab91874ff0f5c6ee0c5434ef5a27
Henry <liaohengyi@outlook.com>
2020-08-12 14:57:29 -0400

    try start

--- out/index.js
+++ out/index.js
@@ -1,14 +1,32 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const start_1 = require("./start");
-start_1.start().then(() => {
-    console.log('Tonva uq-api started!');
-    /*
-    centerApi.queueOut(0, 100).then(value => {
-        console.log(value);
-    }).catch(reason => {
-        console.error(reason);
+(function () {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (!process.env.NODE_ENV) {
+            console.error('NODE_ENV not defined, exit');
+            process.exit();
+        }
+        console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
+        process.exit();
+        yield start_1.start();
+        console.log('Tonva uq-api started!');
+        /*
+        centerApi.queueOut(0, 100).then(value => {
+            console.log(value);
+        }).catch(reason => {
+            console.error(reason);
+        });
+        */
     });
-    */
 });
 //# sourceMappingURL=index.js.map
--- ts/index.ts
+++ ts/index.ts
@@ -1,6 +1,15 @@
 import { start } from "./start";
 
-start().then(() => {
+(async function() {
+
+	if (!process.env.NODE_ENV) {
+		console.error('NODE_ENV not defined, exit');
+		process.exit();
+	}
+	console.log('process.env.NODE_ENV: ', process.env.NODE_ENV);
+
+	process.exit();
+	await start();
     console.log('Tonva uq-api started!');
     /*
     centerApi.queueOut(0, 100).then(value => {
