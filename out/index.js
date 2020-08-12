55bc83ab4d46b7a328753a0f258adec55f6d6158
Henry <liaohengyi@outlook.com>
2020-08-12 15:12:55 -0400

    start

--- out/index.js
+++ out/index.js
@@ -28,5 +28,5 @@ const start_1 = require("./start");
         });
         */
     });
-});
+})();
 //# sourceMappingURL=index.js.map
--- ts/index.ts
+++ ts/index.ts
@@ -18,4 +18,5 @@ import { start } from "./start";
         console.error(reason);
     });
     */
-});
+})();
+
